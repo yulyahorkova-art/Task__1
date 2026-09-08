06397e96d2d7d172fc92b336e26d961246b9cc0d
yulyahorkova-art <yulyahorkova@gmail.com>
2026-09-08 16:56:04 +0300

    Task_1

--- src/test/java/praktikum/IngredientTest.java
+++ src/test/java/praktikum/IngredientTest.java
@@ -3,7 +3,7 @@ package praktikum;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class IngredientTest {
